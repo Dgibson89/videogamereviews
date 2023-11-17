5c46e6b558d721fc2ba48b0b5d301ccf2aa11e41
Dgibson89 <donovan.gibson89@gmail.com>
2023-11-16 22:23:24 -0600

    Large adjustment App.js The endpoint for getting the games was different from getting the screenshots were different so two API are made and the combined into a single variable.Add Review and Reviews are conditionally rendered now as well. Games.js Added comments to remember what is going on here and changed the list item display for style. Games.css Styling for everything regarding when a selected game has been added. I will adjust accordingly to fix the responsive behavior.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Games from "./components/Games";
-import AddReview from "./components/AddReview";
-import Reviews from "./components/Reviews";
+import React, { useState } from "react";
+import Header from "./components/Header"; // Import Header component
+import Games from "./components/Games"; // Import Games component
+import AddReview from "./components/AddReview"; // Import AddReview component
+import Reviews from "./components/Reviews"; // Import Reviews component
+import Footer from "./components/Footer"; // Import Footer component
 
 function App() {
+  const [selectedGame, setSelectedGame] = useState(null);
+
+  // Function to handle game selection and fetch game details
+  const handleGameSelect = async (gameId) => {
+    try {
+      // Fetching game details
+      const detailsResponse = await fetch(
+        `https://api.rawg.io/api/games/${gameId}?key=f3ee281e8bd847168906de9894a8d5af`
+      );
+      const detailsData = await detailsResponse.json();
+
+      // Fetching game screenshots
+      const screenshotsResponse = await fetch(
+        `https://api.rawg.io/api/games/${gameId}/screenshots?key=f3ee281e8bd847168906de9894a8d5af`
+      );
+      const screenshotsData = await screenshotsResponse.json();
+
+      // Combine details and screenshots into one object
+      const selectedGameData = {
+        ...detailsData,
+        screenshots: screenshotsData.results, // Assuming 'results' contains the screenshots array
+      };
+
+      setSelectedGame(selectedGameData);
+    } catch (error) {
+      console.error("Error fetching game details and screenshots:", error);
+    }
+  };
+
   const appStyle = {
     display: "grid",
     gridTemplateRows: "auto 1fr auto", // Header, content, footer
@@ -15,11 +44,33 @@ function App() {
   return (
     <div style={appStyle}>
       <Header />
-      <main>
-        <Games />
-        <AddReview />
-        <Reviews />
-      </main>
+      <Games onGameSelect={handleGameSelect} />
+      {selectedGame && (
+        <div>
+          <div className="selected-game-container">
+            <img src={selectedGame.background_image} alt={selectedGame.name} />
+            <h2>{selectedGame.name}</h2>
+            <div className="game-screenshots">
+              {selectedGame && selectedGame.screenshots && (
+                <div className="game-screenshots">
+                  {selectedGame.screenshots.map((screenshot) => (
+                    <img
+                      key={screenshot.id}
+                      src={screenshot.image}
+                      alt="Screenshot"
+                    />
+                  ))}
+                </div>
+              )}
+            </div>
+            <div className="game-details">
+              <p>{selectedGame.description_raw}</p>
+            </div>
+          </div>
+          <AddReview gameId={selectedGame.id} />
+          <Reviews gameId={selectedGame.id} />
+        </div>
+      )}
       <Footer />
     </div>
   );
--- src/components/Games.js
+++ src/components/Games.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/Games.css'
+import React, { useState, useEffect } from "react";
+import "../styles/Games.css";
 
 function Games({ onGameSelect }) {
   const [games, setGames] = useState([]);
   const [search, setSearch] = useState('');
 
+  // Fetches games on component mount
   useEffect(() => {
     async function fetchGames() {
       try {
-        const response = await fetch(`https://api.rawg.io/api/games?key=YOUR_API_KEY`);
+        const response = await fetch(`https://api.rawg.io/api/games?key=f3ee281e8bd847168906de9894a8d5af`);
         const data = await response.json();
-        setGames(data.results.slice(0, 10));
+        setGames(data.results.slice(0, 10)); // Take first 10 games for initial display
       } catch (error) {
         console.error('Error fetching games:', error);
       }
@@ -18,43 +19,37 @@ function Games({ onGameSelect }) {
     fetchGames();
   }, []);
 
+  // Handles search input changes
   const handleSearchChange = (event) => {
     setSearch(event.target.value);
   };
-  
+
+  // Performs the search operation
   const handleSearch = async () => {
     try {
       const response = await fetch(`https://api.rawg.io/api/games?search=${search}&key=f3ee281e8bd847168906de9894a8d5af`);
       const data = await response.json();
-      setGames(data.results);
+      setGames(data.results); // Update the games list based on search results
     } catch (error) {
       console.error('Error searching games:', error);
     }
   };
 
   return (
-    
-      <div>
-    <div className="search-container">
-      <input 
-        type="text" 
-        value={search} 
-        onChange={handleSearchChange} 
-        className="search-input" 
-        placeholder="Search games..." 
-      />
-      <button onClick={handleSearch} className="search-button">Search</button>
+    <div>
+      <div className="search-container">
+        <input type="text" value={search} onChange={handleSearchChange} className="search-input" />
+        <button onClick={handleSearch} className="search-button">Search</button>
+      </div>
+      <div className="games-grid">
+        {games.map(game => (
+          <div key={game.id} className="game-item" onClick={() => onGameSelect(game.id)}>
+            <img src={game.background_image} alt={game.name} className="game-img" />
+            <p className="game-title">{game.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
-
-    <div className="games-grid">
-      {games.map(game => (
-        <li key={game.id} className="game-item" onClick={() => onGameSelect(game.id)}>
-          <img src={game.background_image} alt={game.name} className="game-img" />
-          <p className="game-title">{game.name}</p>
-        </li>
-      ))}
-    </div>
-  </div>
   );
 }
 
