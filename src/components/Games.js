77e6f9905f7440a899d4e788820857b9f35cd8c8
Dgibson89 <donovan.gibson89@gmail.com>
2023-11-19 00:53:06 -0600

    Games.js Moved the function to fetch games outside the useEffect so the code to reset the games searched for could have access to it. I also added the reset button and the functionality for it.

--- src/components/Games.js
+++ src/components/Games.js
@@ -5,19 +5,22 @@ function Games({ onGameSelect }) {
   const [games, setGames] = useState([]);
   const [search, setSearch] = useState('');
 
+  async function fetchGames() {
+    try {
+      const response = await fetch(`https://api.rawg.io/api/games?key=f3ee281e8bd847168906de9894a8d5af`);
+      const data = await response.json();
+      setGames(data.results.slice(0, 16)); // Take first 16 games for initial display
+    } catch (error) {
+      console.error('Error fetching games:', error);
+    }
+  }
+
+
   // Fetches games on component mount
   useEffect(() => {
-    async function fetchGames() {
-      try {
-        const response = await fetch(`https://api.rawg.io/api/games?key=f3ee281e8bd847168906de9894a8d5af`);
-        const data = await response.json();
-        setGames(data.results.slice(0, 10)); // Take first 10 games for initial display
-      } catch (error) {
-        console.error('Error fetching games:', error);
-      }
-    }
     fetchGames();
   }, []);
+  
 
   // Handles search input changes
   const handleSearchChange = (event) => {
@@ -35,11 +38,18 @@ function Games({ onGameSelect }) {
     }
   };
 
+    // Handles reset action
+    const handleReset = () => {
+      setSearch('');
+      fetchGames(); // Fetch initial games list again after reset
+    };
+
   return (
     <div>
       <div className="search-container">
         <input type="text" value={search} onChange={handleSearchChange} className="search-input" />
         <button onClick={handleSearch} className="search-button">Search</button>
+        <button onClick={handleReset} className="reset-button">Reset</button> 
       </div>
       <div className="games-grid">
         {games.map(game => (
