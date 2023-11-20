9daac9bf7869517499af4c5e26253e2925c2d4fe
Dgibson89 <donovan.gibson89@gmail.com>
2023-11-20 14:42:09 -0600

    AddReview.js
    Adjusted the return to include the stylings created in the css file.

--- src/components/AddReview.js
+++ src/components/AddReview.js
@@ -1,93 +1,66 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import "../styles/AddReview.css";
 
 function AddReview() {
-    const [formData, setFormData] = useState({
-      game: '', // assuming game is identified by an ID or name
-      user: '',
-      rating: '',
-      comment: ''
-    });
-  
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormData(prevFormData => ({
-          ...prevFormData,
-          [name]: value
-        }));
-      };
-      
-      const handleSubmit = async (event) => {
-        event.preventDefault();
-      
-        try {
-          const response = await fetch('/api/reviews', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData)
-          });
-      
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          } else {
-            // Handle successful form submission here
-            console.log("Review added successfully");
-            // Optionally, clear the form
-            setFormData({ game: '', user: '', rating: '', comment: '' });
-          }
-        } catch (error) {
-          console.error('Error submitting form:', error);
-        }
-      };
-      
-  
-      return (
-        <div>
-          <h2>Add a Review</h2>
-          <form onSubmit={handleSubmit}>
-            <label>
-              Game:
-              <input
-                type="text"
-                name="game"
-                value={formData.game}
-                onChange={handleChange}
-              />
-            </label>
-            <label>
-              User:
-              <input
-                type="text"
-                name="user"
-                value={formData.user}
-                onChange={handleChange}
-              />
-            </label>
-            <label>
-              Rating:
-              <input
-                type="number"
-                name="rating"
-                value={formData.rating}
-                onChange={handleChange}
-              />
-            </label>
-            <label>
-              Comment:
-              <textarea
-                name="comment"
-                value={formData.comment}
-                onChange={handleChange}
-              />
-            </label>
-            <button type="submit">Submit Review</button>
-          </form>
-        </div>
-      );
-      
-  }
-  
-  export default AddReview;
-  
+  const [formData, setFormData] = useState({
+    game: "", // assuming game is identified by an ID or name
+    user: "",
+    rating: "",
+    comment: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("/api/reviews", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      } else {
+        // Handle successful form submission here
+        console.log("Review added successfully");
+        // Optionally, clear the form
+        setFormData({ game: "", user: "", rating: "", comment: "" });
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
+
+  return (
+    <div className="add-review-container">
+      <h2 className="add-review-title">Write a Review</h2>
+      <form className="add-review-form">
+        <input
+          type="text"
+          className="add-review-input"
+          placeholder="Review Title"
+        />
+        <textarea
+          className="add-review-textarea"
+          placeholder="Enter Review"
+        ></textarea>
+        <button type="submit" className="add-review-button">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default AddReview;
